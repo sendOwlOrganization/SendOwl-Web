467ba50703bb9153bd080f791f8ffa52ec448b2f
Sangbin Lee <leesangbin@outlook.com>
2022-04-14 15:15:49 +0200

    feat: add mui package

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,36 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { CacheProvider } from '@emotion/react';
+import { EmotionCache } from '@emotion/utils';
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { useState } from 'react';
 import { RecoilRoot } from 'recoil';
+import createEmotionCache from '../src/mui/createEmotionCache';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-      <RecoilRoot>
-        <Component {...pageProps} />
-      </RecoilRoot>
-  )
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+    emotionCache?: EmotionCache;
 }
 
-export default MyApp
+function MyApp({ Component, pageProps, emotionCache = clientSideEmotionCache }: MyAppProps) {
+    const [theme, setTheme] = useState(createTheme());
+
+    return (
+        <CacheProvider value={emotionCache}>
+            <Head>
+                <meta name="viewport" content="initial-scale=1, width=device-width"/>
+            </Head>
+            <RecoilRoot>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline/>
+                    <Component {...pageProps} />
+                </ThemeProvider>
+            </RecoilRoot>
+        </CacheProvider>
+    );
+}
+
+export default MyApp;
---
+++ pages/_document.tsx
@@ -0,0 +1,58 @@
+import createEmotionServer from '@emotion/server/create-instance';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+import createEmotionCache from '../src/mui/createEmotionCache';
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link rel="shortcut icon" href="/static/favicon.ico"/>
+                    <link
+                        rel="stylesheet"
+                        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+                    />
+                    {/* Inject MUI styles first to match with the prepend: true configuration. */}
+                    {(this.props as any).emotionStyleTags}
+                </Head>
+                <body>
+                <Main/>
+                <NextScript/>
+                </body>
+            </Html>
+        );
+    }
+}
+
+MyDocument.getInitialProps = async (ctx) => {
+    const originalRenderPage = ctx.renderPage;
+
+    const cache = createEmotionCache();
+    const { extractCriticalToChunks } = createEmotionServer(cache);
+
+    ctx.renderPage = () =>
+        originalRenderPage({
+            enhanceApp: (App: any) =>
+                function EnhanceApp(props) {
+                    return <App emotionCache={cache} {...props} />;
+                },
+        });
+
+    const initialProps = await Document.getInitialProps(ctx);
+    // This is important. It prevents emotion to render invalid HTML.
+    // See https://github.com/mui/material-ui/issues/26561#issuecomment-855286153
+    const emotionStyles = extractCriticalToChunks(initialProps.html);
+    const emotionStyleTags = emotionStyles.styles.map((style) => (
+        <style
+            data-emotion={`${style.key} ${style.ids.join(' ')}`}
+            key={style.key}
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{ __html: style.css }}
+        />
+    ));
+
+    return {
+        ...initialProps,
+        emotionStyleTags,
+    };
+};
