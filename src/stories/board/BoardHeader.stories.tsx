4f4b3b5346e43e06e8d13e3e787b39792723372d
Sangbin Lee <leesangbin@outlook.com>
2022-04-22 00:40:16 +0200

    story: add example stories for Board

---
+++ src/stories/board/BoardHeader.stories.tsx
@@ -0,0 +1,18 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import BoardHeader from '../../components/board/BoardHeader';
+
+export default {
+    title: 'components/Board/sub-components',
+    component: BoardHeader,
+} as ComponentMeta<typeof BoardHeader>;
+
+const Template: ComponentStory<typeof BoardHeader> = (args) => <BoardHeader {...args}/>;
+
+export const Header = Template.bind({});
+Header.args = {
+    title: '제목 입니다',
+    date: '2022-04-22',
+    author: '부엉이',
+    pageView: 4591,
+    commentCount: 201,
+};
