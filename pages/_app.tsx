e4f9aa8aeb4c87c163e642d58941b28db8b3d4bf
LeeSuHa98 <lapi12@naver.com>
2022-04-28 08:46:40 +0900

    add: global layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,11 @@ import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { useState } from 'react';
+
 import { RecoilRoot } from 'recoil';
 import createEmotionCache from '../src/mui/createEmotionCache';
 import '../styles/globals.css';
+import { GlobalLayout } from '../src/components/global';
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -14,19 +16,27 @@ const clientSideEmotionCache = createEmotionCache();
 interface MyAppProps extends AppProps {
     emotionCache?: EmotionCache;
 }
-
-function MyApp({ Component, pageProps, emotionCache = clientSideEmotionCache }: MyAppProps) {
+function MyApp({
+    Component,
+    pageProps,
+    emotionCache = clientSideEmotionCache,
+}: MyAppProps) {
     const [theme, setTheme] = useState(createTheme());
 
     return (
         <CacheProvider value={emotionCache}>
             <Head>
-                <meta name="viewport" content="initial-scale=1, width=device-width"/>
+                <meta
+                    name='viewport'
+                    content='initial-scale=1, width=device-width'
+                />
             </Head>
             <RecoilRoot>
                 <ThemeProvider theme={theme}>
-                    <CssBaseline/>
-                    <Component {...pageProps} />
+                    <CssBaseline />
+                    <GlobalLayout>
+                        <Component {...pageProps} />
+                    </GlobalLayout>
                 </ThemeProvider>
             </RecoilRoot>
         </CacheProvider>
