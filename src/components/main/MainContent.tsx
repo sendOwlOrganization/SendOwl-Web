0ccaada89568112d3369f5f6ef89a15dacaf4e01
LeeSuHa98 <lapi12@naver.com>
2022-05-07 20:48:31 +0900

    refactor: extract MainContent

--- src/components/main/MainContent.tsx
+++ src/components/main/MainContent.tsx
@@ -1,6 +1,35 @@
 import * as React from 'react';
-import Content from './Content';
+import BoardCategoryList from '../board-category-list/BoardCategoryList';
 
-export default function MainContent() {
-    return <Content />;
+interface BoardCategory {
+    id: number;
+    name: string;
+}
+interface Board {
+    id: number;
+    category: string;
+    title: string;
+    likeCount: number;
+    commentCount: number;
+}
+interface BoardCategoryListProps {
+    categorys: BoardCategory[];
+    boards: Board[];
+}
+
+export default function MainContent({
+    categorys,
+    boards,
+}: BoardCategoryListProps) {
+    return (
+        <>
+            {categorys.map((category) => (
+                <BoardCategoryList
+                    key={category.id}
+                    title={category.name}
+                    boards={boards}
+                />
+            ))}
+        </>
+    );
 }
