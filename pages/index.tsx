786ed9ba3e8a91401bee88039d4ac202848f5cac
Sangbin Lee <leesangbin@outlook.com>
2022-05-21 01:35:22 +0200

    feat: get boards by category from back

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,17 @@
 import { Grid } from '@mui/material';
 import { ComponentProps } from 'react';
+import { getBoards } from '../src/api';
 import BoardCategoryList from '../src/components/board-category-list/BoardCategoryList';
 import { Search } from '../src/components/search';
-import { boardsMock } from '../src/mocks/boards';
+import { BoardCategory, getBoardCategoryName } from '../src/types/Category';
+
+type Boards = ComponentProps<typeof BoardCategoryList>['boards'];
 
 interface HomePageProps {
-    boards: ComponentProps<typeof BoardCategoryList>['boards'];
+    boardsByCategory: Record<number, Boards>;
 }
 
-const Home = ({ boards }: HomePageProps) => {
+const Home = ({ boardsByCategory }: HomePageProps) => {
     return (
         <>
             <Grid container>
@@ -16,17 +19,43 @@ const Home = ({ boards }: HomePageProps) => {
                     <Search />
                 </Grid>
             </Grid>
-            <BoardCategoryList title={'토픽 베스트'} boards={boards} />
-            <BoardCategoryList title={'자유 게시판'} boards={boards} />
-            <BoardCategoryList title={'익명 게시판'} boards={boards} />
+            {
+                Object.entries(boardsByCategory).map(([categoryId, boards]) => <BoardCategoryList key={categoryId}
+                                                                                                  title={categoryId}
+                                                                                                  boards={boards} />)
+            }
         </>
     );
 };
 
-export const getStaticProps = async () => {
+export const getServerSideProps = async () => {
+    const categories = [
+        BoardCategory.FREE,
+        BoardCategory.CAREER,
+        BoardCategory.JOB,
+        BoardCategory.STUDY,
+        BoardCategory.INTERVIEW,
+    ];
+
+    const boards = await Promise.all(categories.map(categoryId => getBoards(0, 10, categoryId)));
+
+    const boardsByCategory: Record<string, Boards> = {};
+    boards.forEach(({ data, error }, categoryIndex) => {
+        if (!data?.boards)
+            return;
+        const categoryName = getBoardCategoryName(categories[categoryIndex]);
+        boardsByCategory[categoryName] = data.boards.map((b) => ({
+            id: b.id,
+            category: categoryName,
+            title: b.title,
+            commentCount: -1, // FIXME
+            likeCount: -1, // FIXME
+        }));
+    });
+
     return {
         props: {
-            boards: boardsMock,
+            boardsByCategory: boardsByCategory,
         },
     };
 };
