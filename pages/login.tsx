fbe7710b6b76f791e6c1d46ca9a95726ffee6a60
Sangbin Lee <leesangbin@outlook.com>
2022-06-24 22:24:50 +0900

    refactor: remove unused provider

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,18 +1,15 @@
 import { Button, Typography } from '@mui/material';
-import { BuiltInProviderType } from 'next-auth/providers';
-import { ClientSafeProvider, getProviders, signOut, useSession } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
 import LoginForm from '../src/components/login/LoginForm';
 import SocialLoginButtons from '../src/components/login/SocialLoginButtons';
 
 interface LoginPageProps {
-    providers: Record<BuiltInProviderType, ClientSafeProvider>;
 }
 
 
-const LoginPage = ({ providers }: LoginPageProps) => {
+const LoginPage = ({}: LoginPageProps) => {
     const session = useSession();
 
-    console.log(session);
     return (
         <>
             <Typography component={'pre'}>{JSON.stringify(session, null, 2)}</Typography>
@@ -23,12 +20,5 @@ const LoginPage = ({ providers }: LoginPageProps) => {
     );
 };
 
-export async function getServerSideProps() {
-    return {
-        props: {
-            providers: await getProviders(),
-        },
-    };
-}
 
 export default LoginPage;
