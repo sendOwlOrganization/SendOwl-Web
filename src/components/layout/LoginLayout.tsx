3aaeb1ae8cd0bd15e3c3c05257f872e74d202c56
Sangbin Lee <leesangbin@outlook.com>
2022-07-19 20:56:15 +0900

    style: fix login layout style to not overflow vh

--- src/components/layout/LoginLayout.tsx
+++ src/components/layout/LoginLayout.tsx
@@ -5,7 +5,7 @@ import { PropsWithChildren } from 'react';
 const StyledImg = styled('img')(({ theme }) => css`
   width: 100%;
   object-fit: scale-down;
-  height: 100vh;
+  height: 100%;
   pointer-events: none;
 
   ${theme.breakpoints.between('xs', 'md')} {
@@ -17,20 +17,18 @@ const StyledImg = styled('img')(({ theme }) => css`
 const LoginLayout = ({ children }: PropsWithChildren<{}>) => {
 
     return (
-        <Box padding={{ xs: 1, sm: 1 }}>
-            <Box mt={1} mb={1}>
-                <Grid container alignItems={'center'} justifyContent={'center'}>
-                    <Grid item xs={12} sm={12} md={5}>
-                        <StyledImg
-                            src={'https://www.jobaba.net/file/image.do?filePath=6oDTW4G6buSBG55/UOZx1+BAa9iPvr9UELTjz5GYKvM8tgetS5cZHHLCcD2FcQAn'}
-                            alt={'test'} />
+        <Box paddingLeft={{ xs: 1, sm: 1 }} paddingRight={{ xs: 1, sm: 1 }}>
+            <Grid sx={{ minHeight: '100vh' }} container alignItems={'center'} justifyContent={'center'}>
+                <Grid item xs={12} sm={12} md={5}>
+                    <StyledImg
+                        src={'https://www.jobaba.net/file/image.do?filePath=6oDTW4G6buSBG55/UOZx1+BAa9iPvr9UELTjz5GYKvM8tgetS5cZHHLCcD2FcQAn'}
+                        alt={'test'} />
 
-                    </Grid>
-                    <Grid xs={12} sm={12} md={7}>
-                        {children}
-                    </Grid>
                 </Grid>
-            </Box>
+                <Grid xs={12} sm={12} md={7}>
+                    {children}
+                </Grid>
+            </Grid>
         </Box>
     );
 };
