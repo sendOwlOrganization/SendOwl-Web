f0fa9ba9b03236fd527ec4af8ff4255570adb747
LeeSuHa98 <lapi12@naver.com>
2022-07-26 19:52:12 +0900

    add: add typography theme

--- styles/sendOwlTheme.ts
+++ styles/sendOwlTheme.ts
@@ -1,29 +1,37 @@
 import { createTheme } from '@mui/material';
 
-const createSendOwlTheme = () => createTheme({
-    palette: {
-        primary: {
-            main: 'rgba(125,178,255,1)',
+const createSendOwlTheme = () =>
+    createTheme({
+        palette: {
+            primary: {
+                main: '#B488F8',
+            },
+            secondary: {
+                main: '#83D9A6',
+            },
         },
-        secondary: {
-            main: 'rgba(40,35,206,1)',
+        shape: {
+            borderRadius: 0,
         },
-    },
-    shape: {
-        borderRadius: 0,
-    },
-    components: {
-        MuiButton: {
-            styleOverrides: {
-                contained: {
-                    color: 'white',
-                    background: 'linear-gradient(239deg, rgba(40,35,206,1) 0%, rgba(125,178,255,1) 100%)',
-                    border: '1px solid',
-                    borderImage: 'linear-gradient(239deg, rgba(40,35,206,1) 0%, rgba(125,178,255,1) 100%) 1',
+        typography: {
+            fontFamily: [
+                'Noto Sans KR'
+            ].join(','),
+        },
+        components: {
+            MuiButton: {
+                styleOverrides: {
+                    contained: {
+                        color: 'white',
+                        background:
+                            'linear-gradient(239deg, rgba(80,35,206,1) 0%, rgba(125,178,255,1) 100%)',
+                        border: '1px solid',
+                        borderImage:
+                            'linear-gradient(239deg, rgba(40,35,206,1) 0%, rgba(125,178,255,1) 100%) 1',
+                    },
                 },
             },
         },
-    },
-});
+    });
 
 export default createSendOwlTheme;
