94dc990ffabb002a3a204ac4535d2a971bfd43f1
LeeSuHa98 <lapi12@naver.com>
2022-07-26 19:49:48 +0900

    refactor: global components code

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -1,8 +1,8 @@
-import { Grid, useTheme } from '@mui/material';
+import { Grid, useTheme, styled, Box } from '@mui/material';
 import { Search } from '../search';
-import HeaderMenu from './HeaderMenu';
 import Image from 'next/image';
-import SideBar from './Sidebar';
+import logo from '../../../public/images/logo.png';
+import UserProfile from '../../component/atoms/UserProfile';
 
 const MainHeader = () => {
     const theme = useTheme();
@@ -10,11 +10,23 @@ const MainHeader = () => {
     return (
         <>
             <Grid
-                sx={{ display: { lg: 'flex' }, backgroundColor: 'white' }}
-                width={'1050px'}
-                marginLeft={'200px'}
-                position={'fixed'}>
-                <HeaderMenu />
+                display={'flex'}
+                height={70}
+                justifyContent={'space-between'}
+                alignItems={'center'}
+                marginTop={'30px'}>
+                <Grid
+                    sx={{
+                        display: 'flex',
+                        marginLeft: '60px',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                    alignItems={'baseline'}>
+                    <Image src={logo} alt='logo' width={55} height={55} />
+                </Grid>
+                <Search />
+                <UserProfile size={65} />
             </Grid>
         </>
     );
