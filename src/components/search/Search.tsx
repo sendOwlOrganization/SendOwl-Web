785d40bb47cd1c9235a23daa36365af504e5491c
LeeSuHa98 <lapi12@naver.com>
2022-07-26 19:50:58 +0900

    refactor: search component code

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,65 +1,44 @@
 import CloseIcon from '@mui/icons-material/Close';
 import SearchIcon from '@mui/icons-material/Search';
-import { Grid, IconButton, InputBase, Stack } from '@mui/material';
+import { Grid, IconButton, InputBase, Stack, useTheme } from '@mui/material';
 
 const Search = () => {
+    const theme = useTheme();
+
     return (
         <>
-            <Grid
-                flex='1'
-                container
-                justifyContent={'flex-end'}
-                sx={{
-                    alignItems: 'center',
-                    display: { lg: 'none', xs: 'block' },
-                }}>
-                <IconButton
-                    component='form'
-                    sx={{
-                        display: {
-                            lg: 'none',
-                            xs: 'block',
-                        },
-                    }}>
-                    <SearchIcon
-                        sx={{
-                            color: '#111111',
-                            fontSize: '30px',
-                            display: 'flex',
-                        }}
-                    />
-                </IconButton>
-            </Grid>
-
             <Stack
                 component='form'
                 sx={{
-                    p: '2px 4px',
+                    p: '1px 15px',
                     display: {
                         lg: 'block',
                         xs: 'none',
                     },
                     alignItems: 'center',
-                    border: 2,
-                    borderRadius: '25px',
-                    margin: '20px 0 20px 0',
+                    borderRadius: '20px',
+                    margin: '10px 0px 10px 0px',
+                    border: `1px solid ${theme.palette.grey[200]}`,
+                    bgcolor: 'white',
                 }}
+                width='783px'
                 display='flex'>
+                <InputBase
+                    sx={{
+                        ml: 1,
+                        flex: 1,
+                        minWidth: '93%',
+                        minHeight: '65px',
+                    }}
+                    inputProps={{
+                        placeholder: 'search...',
+                    }}
+                />
                 <IconButton
                     type='submit'
                     sx={{ p: '10px', flex: 1 }}
                     aria-label='search'>
-                    <SearchIcon sx={{ color: '#111111' }} />
-                </IconButton>
-                <InputBase
-                    sx={{ ml: 1, flex: 1, minWidth: '90%' }}
-                    inputProps={{
-                        placeholder: '관심있는 내용을 검색해보세요 !',
-                        style: { fontWeight: 'bold' },
-                    }}
-                />
-                <IconButton sx={{ color: '#111111' }}>
-                    <CloseIcon />
+                    <SearchIcon sx={{ color: 'grey' }} />
                 </IconButton>
             </Stack>
         </>
