6f1707bf0bd3db43a88ac3e2502d2020de99fb37
LeeSuHa98 <lapi12@naver.com>
2022-07-26 19:49:48 +0900

    refactor: global components code

--- src/components/global/HeaderMenu.tsx
+++ src/components/global/HeaderMenu.tsx
@@ -12,63 +12,7 @@ const GNB = styled(Grid)<GridProps>(({ theme }) => ({
 }));
 
 const Menu = () => {
-    return (
-        <>
-            <Stack
-                spacing={10}
-                direction='row'
-                sx={{
-                    display: { lg: 'flex', xs: 'none', fontWeight: '600' },
-                }}
-                justifyContent='center'
-                alignItems='center'
-                padding={1}
-                marginLeft={'30px'}
-                marginTop={'20px'}
-                fontSize={'17px'}>
-                <GNB>
-                    <Link href='/total-board'>
-                        <a>전체 게시판</a>
-                    </Link>
-                </GNB>
-                <GNB>
-                    <Link href='/board'>
-                        <a>일반 게시판</a>
-                    </Link>
-                </GNB>
-                <GNB>
-                    <Link href='/mbti-board'>
-                        <a>MBTI 게시판</a>
-                    </Link>
-                </GNB>
-                <GNB>
-                    <Link href='/faq'>
-                        <a>FAQ</a>
-                    </Link>
-                </GNB>
-            </Stack>
-
-            <Stack
-                spacing={4}
-                direction='row'
-                sx={{ display: { lg: 'none', xs: 'flex', fontWeight: '500' } }}
-                alignItems='center'
-                padding={3}>
-                <Link href='/total-board'>
-                    <a>전체 게시판</a>
-                </Link>
-                <Link href='/board'>
-                    <a>일반 게시판</a>
-                </Link>
-                <Link href='/mbti-board'>
-                    <a>MBTI 게시판</a>
-                </Link>
-                <Link href='/faq'>
-                    <a>FAQ</a>
-                </Link>
-            </Stack>
-        </>
-    );
+    return <></>;
 };
 
 export default Menu;
