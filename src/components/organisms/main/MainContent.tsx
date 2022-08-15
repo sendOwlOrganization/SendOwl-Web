3ade135c2c58eda62a6d2670ff36aa18e81378cb
Sangbin Lee <leesangbin@outlook.com>
2022-08-16 00:27:14 +0900

    :bug: add props to MainContent

--- src/components/organisms/main/MainContent.tsx
+++ src/components/organisms/main/MainContent.tsx
@@ -1,15 +1,16 @@
-import {Container, Grid, useTheme, Box, useMediaQuery} from '@mui/material';
+import { Container, Grid, useMediaQuery, useTheme } from '@mui/material';
 import * as React from 'react';
-import { Mbti } from '../chart';
 import Advertisement from '../../molecules/Advertisement';
 import CategoryMenu from '../../molecules/CategoryMenu';
-import CreatePost from '../../molecules/CreatePost';
 import ContentList from '../../molecules/ContentList';
+import CreatePost from '../../molecules/CreatePost';
+import { Mbti } from '../chart';
 
 interface BoardCategory {
     id: number;
     name: string;
 }
+
 interface Board {
     id: number;
     category: string;
@@ -17,12 +18,17 @@ interface Board {
     likeCount: number;
     commentCount: number;
 }
+
 interface BoardCategoryListProps {
     categories: BoardCategory[];
     boards: Board[];
 }
 
-const MainContent = () => {
+interface MainContentProps {
+
+}
+
+const MainContent = ({}: MainContentProps) => {
     const theme = useTheme();
     const isMobile = useMediaQuery("(max-width: 600px)");
     const mobileWidth = 355
@@ -33,10 +39,10 @@ const MainContent = () => {
             <Container maxWidth={'md'} disableGutters={true}>
                 {isMobile ?
                     <>
-                        <Grid margin={'0 10px 0 10px'}  >
-                            <CategoryMenu width={55} height={55} margin={'0 5px 0 5px'}/>
-                            <Advertisement width={mobileWidth} height={137} margin={'10px 0 0 0'}/>
-                            <CreatePost width={mobileWidth}/>
+                        <Grid margin={'0 10px 0 10px'}>
+                            <CategoryMenu width={55} height={55} margin={'0 5px 0 5px'} />
+                            <Advertisement width={mobileWidth} height={137} margin={'10px 0 0 0'} />
+                            <CreatePost width={mobileWidth} />
                             <ContentList width={mobileWidth} title='인기글' />
                             <ContentList width={mobileWidth} title='최신글' />
                         </Grid>
@@ -44,11 +50,11 @@ const MainContent = () => {
                     :
                     <>
                         <Mbti />
-                        <CategoryMenu width={70} height={70} margin={'20px 20px 20px 0'}/>
-                        <Advertisement width={webWidth} height={227}/>
+                        <CategoryMenu width={70} height={70} margin={'20px 20px 20px 0'} />
+                        <Advertisement width={webWidth} height={227} />
                         <CreatePost width={webWidth} />
-                        <ContentList width={webWidth} title='인기글'/>
-                        <ContentList width={webWidth} title='최신글'/>
+                        <ContentList width={webWidth} title='인기글' />
+                        <ContentList width={webWidth} title='최신글' />
                     </>
                 }
             </Container>
