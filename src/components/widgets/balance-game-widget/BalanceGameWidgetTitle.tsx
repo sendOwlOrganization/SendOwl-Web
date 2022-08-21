abfe2b49a8ee99567d44d195fa82453766200aa7
Sangbin Lee <leesangbin@outlook.com>
2022-08-22 01:44:55 +0900

    :lipstick: fix style when question mark is not present

--- src/components/widgets/balance-game-widget/BalanceGameWidgetTitle.tsx
+++ src/components/widgets/balance-game-widget/BalanceGameWidgetTitle.tsx
@@ -10,16 +10,12 @@ const BalanceGameWidgetTitle = ({ id, title }: BalanceGameWidgetTitleProps) => {
     return (
         <BannerTitle href={`#?FIXME_LINK_ID=${id}`}>
             <Typography variant={'body1'} fontWeight={'bold'}>
-                {
-                    title.endsWith('?') && (
-                        <Typography variant={'body1'}
-                                    fontWeight={'bold'}
-                                    component={'span'}
-                                    color={theme => theme.palette.purple[600]}>
-                            {'Q. '}
-                        </Typography>
-                    )
-                }
+                <Typography variant={'body1'}
+                            fontWeight={'bold'}
+                            component={'span'}
+                            color={theme => theme.palette.purple[600]}>
+                    {'Q. '}
+                </Typography>
                 {title}
             </Typography>
         </BannerTitle>
