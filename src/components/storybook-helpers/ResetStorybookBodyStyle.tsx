135c0d2b6cfcc5c2f19f64ad15779e832aa8c27e
Sangbin Lee <leesangbin@outlook.com>
2022-08-23 16:08:26 +0900

    :sparkles: add storybook helpers

---
+++ src/components/storybook-helpers/ResetStorybookBodyStyle.tsx
@@ -0,0 +1,11 @@
+import css from '@emotion/css';
+import { Global } from '@emotion/react';
+
+export const ResetStorybookBodyStyle = () => (
+    <Global styles={css`
+      body {
+        padding: 0 !important;
+      }`} />
+);
+
+export default ResetStorybookBodyStyle;
