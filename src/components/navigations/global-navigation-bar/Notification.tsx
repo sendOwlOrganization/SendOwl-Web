f08e2187e649683cc57f5f9a2dca5bf4009820c0
Sangbin Lee <leesangbin@outlook.com>
2022-08-23 16:08:50 +0900

    :sparkles: add GNB

---
+++ src/components/navigations/global-navigation-bar/Notification.tsx
@@ -0,0 +1,9 @@
+import NotificationIcon from '@components/icons/NotificationIcon';
+
+const Notification = () => {
+    return (
+        <NotificationIcon color={'black'} clickable disableHoverBackground badge={'lightPink'} />
+    );
+};
+
+export default Notification;
---
+++ src/components/navigations/global-navigation-bar/User.tsx
@@ -0,0 +1,9 @@
+import UserIcon from '@components/icons/UserIcon';
+
+const User = () => {
+    return (
+        <UserIcon color={'black'} clickable disableHoverBackground />
+    );
+};
+
+export default User;
