3b5043b321f8ff23d884cd37b3b06d34236c3d08
Sangbin Lee <leesangbin@outlook.com>
2022-08-24 17:21:45 +0900

    :recycle: make search icon open SearchModal

--- src/components/navigations/global-navigation-bar/Search.tsx
+++ src/components/navigations/global-navigation-bar/Search.tsx
@@ -1,9 +1,33 @@
 import SearchIcon from '@components/icons/SearchIcon';
+import SearchBox from '@components/search/SearchBox';
+import { Dialog } from '@mui/material';
+import { useState } from 'react';
+
+const searchBoxMocks = {
+    recentlySearched: ['최근', '검색어', '아래', '토글로', '끌 수 있음'],
+    recommendations: ['유저와 같은', 'MBTI가', '많이 검색', '검색어(키워드)', '추천'],
+    populars: [...Array(10).keys()].map(i => `인기 검색어 ${i + 1}`),
+};
 
 const Search = () => {
+    const [open, setOpen] = useState<boolean>(false);
+
+    const handleOpenDialog = () => {
+        setOpen(true);
+    };
+
+    const handleCloseDialog = () => {
+        setOpen(false);
+    };
+
     return (
         <>
-            <SearchIcon color={'black'} clickable disableHoverBackground />
+            <SearchIcon color={open ? 'pink' : 'black'}
+                        onClick={handleOpenDialog}
+                        disableHoverBackground />
+            <Dialog maxWidth={'xl'} open={open} onClose={handleCloseDialog}>
+                <SearchBox {...searchBoxMocks} />
+            </Dialog>
         </>
     );
 };
