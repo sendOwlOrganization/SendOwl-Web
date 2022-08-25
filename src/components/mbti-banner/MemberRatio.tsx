6d4eee10b97a844d82113a87e28e8781387c0e98
Sangbin Lee <leesangbin@outlook.com>
2022-08-25 16:31:26 +0900

    :recycle: extract UserMbti and MemberRatio component

---
+++ src/components/mbti-banner/MemberRatio.tsx
@@ -0,0 +1,22 @@
+import { Typography } from '@mui/material';
+import useCountUp from '@tools/useCountUp';
+
+interface MemberRatioProps {
+    count: number;
+    total: number;
+}
+
+const MemberRatio = ({ count, total }: MemberRatioProps) => {
+    const animatedCount = useCountUp(count, 0, 500);
+    return (
+        <Typography variant={'body2'} lineHeight={1.65} color={theme => theme.palette.gray[700]}>
+            <Typography variant={'body2'} component={'span'} fontWeight={700}
+                        color={'inherit'}>
+                {animatedCount.toLocaleString()}
+            </Typography>
+            {` / ${total.toLocaleString()}`}
+        </Typography>
+    );
+};
+
+export default MemberRatio;
