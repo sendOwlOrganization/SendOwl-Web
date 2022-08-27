2356289aa9fd8f6d22cec203f3720c1c81d9afd4
Sangbin Lee <leesangbin@outlook.com>
2022-08-28 01:20:05 +0900

    :construction: wip login page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { Card, CardContent, Typography } from '@mui/material';
+import NextLink from 'next/link';
 
 interface HomePageProps {
 }
@@ -8,9 +9,15 @@ const Home = ({}: HomePageProps) => {
         <>
             <Card elevation={0}>
                 <CardContent>
-                    <Typography align={'center'} variant={'h1'}>
+                    <Typography align={'center'} variant={'h1'} gutterBottom>
                         홈
                     </Typography>
+                    <NextLink href={'/login'} passHref>
+                        <Typography component={'a'} display={'block'} color={'primary.main'} align={'center'}
+                                    variant={'h2'}>
+                            임시 로그인 페이지 링크
+                        </Typography>
+                    </NextLink>
                 </CardContent>
             </Card>
         </>
