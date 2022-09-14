7d7f472dd98b3751b9173e335613f5a51b9b814d
Sangbin Lee <leesangbin@outlook.com>
2022-09-14 18:15:44 +0200

    :sparkles: add new icons

--- src/components/icons/CloseIcon.tsx
+++ src/components/icons/CloseIcon.tsx
@@ -2,9 +2,10 @@ import SvgIcon, { SvgIconProps } from '@components/icons/SvgIcon';
 
 const CloseIcon = (props: SvgIconProps) => {
     return (
-        <SvgIcon {...props}>
+        <SvgIcon {...props} width={14} height={14}>
             <path xmlns='http://www.w3.org/2000/svg'
-                  d='m12 10.586 4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636l4.95 4.95z' />
+                  d='M7.00023 5.58599L11.9502 0.635986L13.3642 2.04999L8.41423 6.99999L13.3642 11.95L11.9502 13.364L7.00023 8.41399L2.05023 13.364L0.63623 11.95L5.58623 6.99999L0.63623 2.04999L2.05023 0.635986L7.00023 5.58599Z'
+            />
         </SvgIcon>
     );
 };
