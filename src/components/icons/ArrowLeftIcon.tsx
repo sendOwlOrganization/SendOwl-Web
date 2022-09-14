4a2d2f439abe3fb55e26bdcebdd0cef9cf9314f6
Sangbin Lee <leesangbin@outlook.com>
2022-09-14 14:54:05 +0200

    :sparkles: add new icons

---
+++ src/components/icons/ArrowLeftIcon.tsx
@@ -0,0 +1,11 @@
+import SvgIcon, { SvgIconProps } from '@components/icons/SvgIcon';
+
+const ArrowLeftIcon = (props: SvgIconProps) => {
+    return <SvgIcon {...props} width={20} height={20}>
+        <path xmlns='http://www.w3.org/2000/svg'
+              d='M9.02329 10L13.1483 14.125L11.97 15.3033L6.66663 10L11.97 4.69667L13.1483 5.875L9.02329 10Z'
+        />
+    </SvgIcon>;
+};
+
+export default ArrowLeftIcon;
---
+++ src/components/icons/CommunityIcon.tsx
@@ -0,0 +1,10 @@
+import SvgIcon, { SvgIconProps } from '@components/icons/SvgIcon';
+
+const CommunityIcon = (props: SvgIconProps) => {
+    return <SvgIcon {...props} width={20} height={21}>
+        <path xmlns='http://www.w3.org/2000/svg'
+              d='M7.167 15h8.53l1.47 1.154V7.5H18a.833.833 0 0 1 .833.833v11.25l-3.712-2.916H8a.833.833 0 0 1-.833-.834V15zm-2.121-1.667L1.333 16.25V3.333a.833.833 0 0 1 .834-.833h12.5a.833.833 0 0 1 .833.833v10H5.046z' />
+    </SvgIcon>;
+};
+
+export default CommunityIcon;
