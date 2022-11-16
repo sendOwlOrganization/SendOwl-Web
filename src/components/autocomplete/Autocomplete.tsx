fa9f0537a4db462947d5baef0d0dfd224ebe156f
Sangbin Lee <leesangbin@outlook.com>
2022-11-17 00:54:25 +0900

    :sparkles: add Autocomplete component

---
+++ src/components/autocomplete/Autocomplete.tsx
@@ -0,0 +1,51 @@
+import { css, styled, Typography } from '@mui/material';
+import { ReactNode, useMemo } from 'react';
+
+interface AutocompleteProps {
+    query: string;
+    words: string[];
+    renderWord?: (word: string) => ReactNode;
+    notFoundMessage?: string;
+    onClick: (word: string) => void;
+    min?: number;
+}
+
+
+const OrderedList = styled('ol')(({ theme }) => css`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+`);
+
+
+const ListItem = styled('li')(({ theme }) => css`
+  padding: 1rem 1.5rem;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${theme.palette.mode === 'dark'
+            ? theme.palette.gray[1000]
+            : theme.palette.gray[100]};
+  }
+`);
+
+
+const Autocomplete = ({ query, words, onClick, notFoundMessage, renderWord, min = 1 }: AutocompleteProps) => {
+    const elements = useMemo(() => {
+        return words.filter(w => query.length >= min && w.includes(query)).map((word, i) => (
+            <ListItem key={`${word}-${i}`} onClick={() => onClick(word)}>
+                {renderWord ? renderWord(word) : word}
+            </ListItem>
+        ));
+    }, [query, words, onClick, renderWord, min]);
+
+    return (
+        elements.length
+            ? <OrderedList>{elements}</OrderedList>
+            : query.length >= min
+                ? <Typography>{notFoundMessage}</Typography>
+                : null
+    );
+};
+
+export default Autocomplete;
