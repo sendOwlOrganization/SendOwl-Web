6b00f9b817ce7829d0aeabc82fe14169456ab931
Sangbin Lee <leesangbin@outlook.com>
2022-12-25 23:59:47 +0900

    :construction: add set-profile when welcome

--- pages/register/welcome.tsx
+++ pages/register/welcome.tsx
@@ -1,6 +1,9 @@
+import { getGoogleLoginDetails, postSetProfile } from '@api/index';
 import EmptyLayout from '@components/layout/EmptyLayout';
 import RectangleButtonLink from '@components/links/RectangleButtonLink';
 import Welcome from '@components/signup/Welcome';
+import { useSession } from 'next-auth/react';
+import { useEffect } from 'react';
 import { useRecoilValue } from 'recoil';
 import { registerStore } from '../../src/store/registerStore';
 
@@ -9,6 +12,30 @@ interface WelcomePageProps {
 
 const WelcomePage = ({}: WelcomePageProps) => {
     const state = useRecoilValue(registerStore);
+    const session = useSession();
+
+    useEffect(() => {
+        console.log(session);
+        if (session.status !== 'authenticated') {
+            return;
+        }
+        // @ts-ignore
+        getGoogleLoginDetails(session.data.token.accessToken)
+            .then(({ data, headers, error }) => {
+                if (data && !data.alreadySetted && headers && headers['accessToken']) {
+                    postSetProfile({
+                        gender: state.gender,
+                        nickName: state.nickname,
+                        age: state.age,
+                        mbti: state.mbti,
+                    }, headers['accessToken'])
+                        .then(console.log)
+                        .catch(console.error);
+                }
+            });
+
+    }, [session.status]);
+
 
     return (
         <>
