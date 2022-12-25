36046e431040a2ee5664bb5e2c87c71f276dd39a
Sangbin Lee <leesangbin@outlook.com>
2022-12-26 00:05:08 +0900

    :sparkles: add back button

--- pages/register/age.tsx
+++ pages/register/age.tsx
@@ -1,11 +1,19 @@
+import ArrowLeftIcon from '@components/icons/ArrowLeftIcon';
 import EmptyLayout from '@components/layout/EmptyLayout';
 import RectangleButtonLink from '@components/links/RectangleButtonLink';
 import Age from '@components/signup/Age';
+import { styled } from '@mui/material';
 import { useRecoilState } from 'recoil';
 import { registerStore } from '../../src/store/registerStore';
 
 interface AgePageProps {
 }
+
+const Header = styled('header')(({ theme }) => ({
+    padding: '1rem 0.25rem',
+    display: 'flex',
+    alignItems: 'center',
+}));
 
 const AgePage = ({}: AgePageProps) => {
     const [state, setState] = useRecoilState(registerStore);
@@ -16,6 +24,10 @@ const AgePage = ({}: AgePageProps) => {
 
     return (
         <>
+            <Header>
+                <ArrowLeftIcon width={20} height={20} disableHoverBackground clickable
+                               onClick={() => router.back()} />
+            </Header>
             <Age age={state.age} setAge={setAge} />
 
             <RectangleButtonLink href={'/register/welcome'}
--- pages/register/gender.tsx
+++ pages/register/gender.tsx
@@ -1,14 +1,24 @@
+import ArrowLeftIcon from '@components/icons/ArrowLeftIcon';
 import EmptyLayout from '@components/layout/EmptyLayout';
 import RectangleButtonLink from '@components/links/RectangleButtonLink';
 import Gender from '@components/signup/Gender';
+import { styled } from '@mui/material';
+import { useRouter } from 'next/router';
 import { useRecoilState } from 'recoil';
 import { registerStore } from '../../src/store/registerStore';
 
 interface GenderPageProps {
 }
 
+const Header = styled('header')(({ theme }) => ({
+    padding: '1rem 0.25rem',
+    display: 'flex',
+    alignItems: 'center',
+}));
+
 const GenderPage = ({}: GenderPageProps) => {
     const [state, setState] = useRecoilState(registerStore);
+    const router = useRouter();
 
     const setGender = (gender: string) => setState(prev => ({ ...prev, gender }));
 
@@ -16,6 +26,10 @@ const GenderPage = ({}: GenderPageProps) => {
 
     return (
         <>
+            <Header>
+                <ArrowLeftIcon width={20} height={20} disableHoverBackground clickable
+                               onClick={() => router.back()} />
+            </Header>
             <Gender gender={state.gender} setGender={setGender} />
 
             <RectangleButtonLink href={'/register/age'}
