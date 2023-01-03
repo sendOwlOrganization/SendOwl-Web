d34c98b60aef3d24c8fe7ba11e5514e1b556d23e
Sangbin Lee <leesangbin@outlook.com>
2023-01-03 22:17:17 +0900

    :sparkles: add signOut and redirect in register page"

--- pages/register/mbti.tsx
+++ pages/register/mbti.tsx
@@ -1,15 +1,27 @@
+import ArrowLeftIcon from '@components/icons/ArrowLeftIcon';
 import EmptyLayout from '@components/layout/EmptyLayout';
 import RectangleButtonLink from '@components/links/RectangleButtonLink';
 import Mbti from '@components/signup/Mbti';
 import { MBTI_LIST } from '@mocks/mbti';
+import { styled } from '@mui/material';
+import { signOut } from 'next-auth/react';
+import { useRouter } from 'next/router';
 import { useRecoilState } from 'recoil';
 import { registerStore } from '../../src/store/registerStore';
 
 interface MbtiPageProps {
 }
 
+
+const Header = styled('header')(({ theme }) => ({
+    padding: '1rem 0.25rem',
+    display: 'flex',
+    alignItems: 'center',
+}));
+
 const MbtiPage = ({}: MbtiPageProps) => {
     const [state, setState] = useRecoilState(registerStore);
+    const router = useRouter();
     const isValid = ([...MBTI_LIST] as string[]).includes(state.mbti);
 
     const setMbti = (mbti: string) => {
@@ -18,6 +30,10 @@ const MbtiPage = ({}: MbtiPageProps) => {
 
     return (
         <>
+            <Header>
+                <ArrowLeftIcon width={20} height={20} disableHoverBackground clickable
+                               onClick={() => signOut()} />
+            </Header>
             <Mbti mbti={state.mbti} setMbti={setMbti} />
             <RectangleButtonLink href={'/register/gender'}
                                  disabled={!isValid}
