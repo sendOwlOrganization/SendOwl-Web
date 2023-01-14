5a1ef4fbf3eaff9c05730f179957eed69575f678
Sangbin Lee <leesangbin@outlook.com>
2023-01-14 16:16:47 +0900

    :recycle: refactor home page with mocks

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,18 +14,21 @@ import { imageMocks } from '@mocks/images';
 import { Box, Divider, Grid, styled } from '@mui/material';
 import { MLAB_ICON_COLORS, MLAB_OPACITY_PALETTE } from '@styles/mlabTheme';
 import Footer from '@UI/Footer';
+import { ComponentProps, Fragment } from 'react';
 
 interface HomePageProps {
+    // FIXME: 타입 백엔드와 정의 필요
+    communityBoards: ComponentProps<typeof BoardPreviewLink>[];
+    balanceGameBoards: ComponentProps<typeof BoardPreviewLink>[];
+    serviceCenterBoards: ComponentProps<typeof BoardPreviewLink>[];
 }
 
-const ShortcutContainer = styled('div')`
-  display: flex;
-  flex-wrap: wrap;
+const ShortcutContainer = styled('section')`
+  display: grid;
   padding: 0.5rem;
+  gap: 0.5rem;
 
-  & > * {
-    margin: 0.5rem;
-  }
+  grid-template-columns: 1fr 1fr;
 `;
 
 const BoardContainer = styled('div')(({ theme }) => css`
@@ -33,7 +36,7 @@ const BoardContainer = styled('div')(({ theme }) => css`
   margin-bottom: 1rem;
 `);
 
-const Home = ({}: HomePageProps) => {
+const Home = ({ communityBoards, balanceGameBoards, serviceCenterBoards }: HomePageProps) => {
     return (
         <Box>
             <Carousel data={imageMocks} showPagination />
@@ -48,51 +51,28 @@ const Home = ({}: HomePageProps) => {
                                 color={MLAB_ICON_COLORS.notice} />
             </ShortcutContainer>
             <BoardContainer>
-                <BoardBannerTitle title={communityList.community.name} icon={<communityList.community.icon
-                    color={communityList.community.color} />} />
-                <BoardPreviewLink id={1} title={'게시글 제목'}
-                                  preview={'Lorem Ipsum 은 단순히 인쇄 및 조판 산업의 더미 텍스트입니다.'}
-                                  user={{ id: 1, nickName: '유저', mbti: 'ENTJ' }} hasVote={true}
-                                  category={'커뮤니티'} likeCount={123} commentCount={21}
-                                  date={'2023-01-12T17:45:22'} />
-                <Divider />
-                <BoardPreviewLink id={1} title={'게시글 제목'}
-                                  preview={'Lorem Ipsum 은 단순히 인쇄 및 조판 산업의 더미 텍스트입니다.'}
-                                  user={{ id: 1, nickName: '유저', mbti: 'ENTJ' }} hasVote={true}
-                                  category={'커뮤니티'} likeCount={123} commentCount={21}
-                                  date={'2023-01-12T17:45:22'} />
-                <Divider />
-                <BoardPreviewLink id={1} title={'게시글 제목'}
-                                  preview={'Lorem Ipsum 은 단순히 인쇄 및 조판 산업의 더미 텍스트입니다.'}
-                                  user={{ id: 1, nickName: '유저', mbti: 'ENTJ' }} hasVote={true}
-                                  category={'커뮤니티'} likeCount={123} commentCount={21}
-                                  date={'2023-01-12T17:45:22'} />
+                <BoardBannerTitle title={communityList.community.name}
+                                  icon={<communityList.community.icon color={communityList.community.color} />} />
+                {
+                    communityBoards?.map(b => <Fragment key={b.id}>
+                        <BoardPreviewLink {...b} />
+                        <Divider />
+                    </Fragment>)
+                }
             </BoardContainer>
             <BoardContainer>
                 <BoardBannerTitle title={communityList.balanceGame.name}
-                                  icon={<communityList.balanceGame.icon
-                                      color={communityList.balanceGame.color} />} />
-                <BoardPreviewLink id={1} title={'게시글 제목'}
-                                  preview={'Lorem Ipsum 은 단순히 인쇄 및 조판 산업의 더미 텍스트입니다.'}
-                                  user={{ id: 1, nickName: '유저', mbti: 'ENTJ' }} hasVote={false}
-                                  likeCount={123} commentCount={21} date={'2023-01-12T17:45:22'}
-                                  imgSrc={'img'} />
-                <Divider />
-                <BoardPreviewLink id={1} title={'게시글 제목'}
-                                  preview={'Lorem Ipsum 은 단순히 인쇄 및 조판 산업의 더미 텍스트입니다.'}
-                                  user={{ id: 1, nickName: '유저', mbti: 'ENTJ' }} hasVote={false}
-                                  likeCount={123} commentCount={21} date={'2023-01-12T17:45:22'}
-                                  imgSrc={'img'} />
-                <Divider />
-                <BoardPreviewLink id={1} title={'게시글 제목'}
-                                  preview={'Lorem Ipsum 은 단순히 인쇄 및 조판 산업의 더미 텍스트입니다.'}
-                                  user={{ id: 1, nickName: '유저', mbti: 'ENTJ' }} hasVote={false}
-                                  likeCount={123} commentCount={21} date={'2023-01-12T17:45:22'}
-                                  imgSrc={'img'} />
+                                  icon={<communityList.balanceGame.icon color={communityList.balanceGame.color} />} />
+                {
+                    balanceGameBoards?.map(b => <Fragment key={b.id}>
+                        <BoardPreviewLink {...b} />
+                        <Divider />
+                    </Fragment>)
+                }
             </BoardContainer>
             <BoardContainer>
-                <BoardBannerTitle title={communityList.insight.name} icon={<communityList.insight.icon
-                    color={communityList.insight.color} />} />
+                <BoardBannerTitle title={communityList.insight.name}
+                                  icon={<communityList.insight.icon color={communityList.insight.color} />} />
                 <Grid>
                     <MbtiWidget user={{ mbti: 'ENTP' }}
                                 data={[{ id: 'ENTP', value: 1000, label: 'ENTP' }]} />
@@ -102,25 +82,69 @@ const Home = ({}: HomePageProps) => {
                 <BoardBannerTitle title={communityList.serviceCenter.name}
                                   icon={<communityList.serviceCenter.icon
                                       color={communityList.serviceCenter.color} />} />
-                <BoardPreviewLink id={1} title={'게시글 제목'}
-                                  preview={'Lorem Ipsum 은 단순히 인쇄 및 조판 산업의 더미 텍스트입니다.'}
-                                  user={{ id: 1, nickName: '유저', mbti: 'ENTJ' }} hasVote={false}
-                                  likeCount={123} commentCount={21} date={'2023-01-12T17:45:22'} />
-                <Divider />
-                <BoardPreviewLink id={1} title={'게시글 제목'}
-                                  preview={'Lorem Ipsum 은 단순히 인쇄 및 조판 산업의 더미 텍스트입니다.'}
-                                  user={{ id: 1, nickName: '유저', mbti: 'ENTJ' }} hasVote={false}
-                                  likeCount={123} commentCount={21} date={'2023-01-12T17:45:22'} />
-                <Divider />
-                <BoardPreviewLink id={1} title={'게시글 제목'}
-                                  preview={'Lorem Ipsum 은 단순히 인쇄 및 조판 산업의 더미 텍스트입니다.'}
-                                  user={{ id: 1, nickName: '유저', mbti: 'ENTJ' }} hasVote={false}
-                                  likeCount={123} commentCount={21} date={'2023-01-12T17:45:22'} />
+                {
+                    serviceCenterBoards?.map(b => <Fragment key={b.id}>
+                        <BoardPreviewLink {...b} />
+                        <Divider />
+                    </Fragment>)
+                }
             </BoardContainer>
             <SNSButton />
             <Footer />
         </Box>
     );
+};
+
+export const getServerSideProps = async () => {
+    // FIXME: 백엔드 호출
+    const mockUser = { id: 1, nickName: '공격적인 ENTP', mbti: 'ENTP' };
+
+    const communityBoards = [
+        {
+            id: 1,
+            title: 'Sunt caculaes vitare albus, clemens brabeutaes.',
+            preview: 'Quadra de lotus capio, visum abaculus!',
+            user: mockUser,
+            hasVote: true,
+            likeCount: 12,
+            commentCount: 21,
+            date: '2023-01-12T17:45:22',
+            imgSrc: 'img',
+            category: '커뮤니티',
+        },
+        {
+            id: 2,
+            title: '안녕하세요 제목입니다',
+            preview: '동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세',
+            user: mockUser,
+            hasVote: false,
+            likeCount: 999999999999999,
+            commentCount: 2,
+            date: '2023-01-12T17:45:22',
+            imgSrc: 'img',
+            category: '일상',
+        },
+        {
+            id: 3,
+            title: 'ㅎ러우할워훌어ㅏ훌아ㅜㅎㄹ우허ㅏㅜㄹㅇ훌어ㅏㅜ헉둬ㅏ훋가ㅜㅎㄹㅇ라ㅣㅜ히ㅏ둑힉두히ㅜ일후ㅏㄷ구하ㅣㄱ둥헝루허ㅣ루힏ㄱ',
+            preview: 'ㅎ라읗ㄹ워ㅏㅜㅗㄱ두ㅏㅣㅎㄱ다ㅣ르ㅏㅣㄴ으라ㅣㅜ훠루허가ㅜ허ㅜ거ㅏ두니휠웋ㄱ둬하구하ㅓ루ㅗㅓㅏㅜㅅ거ㅏㅜㅎ더ㅜ히ㅏㄱ두힉두하ㅣㅜ러훅사ㅜㅗㅓㅏㅅ궈귀ㅏ두하ㅣㄱ둫긷ㅎㄱㄷㅎㄱㄷㅎㄷㄱㅎ',
+            user: mockUser,
+            hasVote: false,
+            likeCount: 12,
+            commentCount: 21,
+            date: '2023-01-12T17:45:22',
+            imgSrc: 'img',
+            category: '연애',
+        },
+    ];
+
+    return {
+        props: {
+            communityBoards,
+            balanceGameBoards: communityBoards,
+            serviceCenterBoards: communityBoards,
+        },
+    };
 };
 
 export default Home;
